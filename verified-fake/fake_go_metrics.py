229422ab41b2f3291bce8809a164f6efb468e5bb
justinvdm <justinvdm.0.1.0@gmail.com>
2014-09-17 12:15:58 +0200

    Document verified fake's purpose

--- verified-fake/fake_go_metrics.py
+++ verified-fake/fake_go_metrics.py
@@ -1,4 +1,10 @@
+"""
+A verified fake implementation of go-metrics for use in tests.
+
+This implementation is tested in the go-metrics package alongside the API it is
+faking, to ensure that the behaviour is the same for both.
+"""
+
+
 class FakeMetricsApi(object):
-    """
-    Fake implementation of the metrics API.
-    """
+    pass
